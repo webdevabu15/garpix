79ee1529f6b6f8a5c46fb329f0c900a3f2b43e95
moorodjohn101 <moorodjohn101@gmail.com>
2024-06-26 19:09:54 +0500

    hamburger button to'g'rilandi

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,38 +1,40 @@
-import { Menu, Flex, Burger } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
-import "@mantine/core/styles.css"
-import { Link } from 'react-router-dom';
-import {navLinkGroup,navLinkGroup2 } from "../constants.ts"
+import { Menu, Flex, Burger } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import "@mantine/core/styles.css";
+import { Link } from "react-router-dom";
+import { navLinkGroup, navLinkGroup2 } from "../constants.ts";
 function Demo() {
-  const [opened, { toggle }] = useDisclosure();
+  const [opened, { toggle, close }] = useDisclosure();
+  console.log(opened);
   return (
-    <Menu shadow="md" width={300} closeOnClickOutside={true ? opened == false : opened == true}>
+    <Menu shadow="md" width={300} onClose={close}>
       <Menu.Target>
-        <Burger opened={opened} onClick={toggle} ria-label="Toggle navigation" className="nav-toggler"></Burger>
+        <Burger
+          opened={opened}
+          onClick={toggle}
+          ria-label="Toggle navigation"
+          className="nav-toggler"
+        />
       </Menu.Target>
 
-      <Menu.Dropdown  trigger="click-hover" closeDelay={0}>
-        <Flex fz={'18px'} direction={'column'} pl={20} pr={20}>
-            {navLinkGroup.map((link) => (
-              <li className="nav-link-item">
-                <Link to={"/singlePage"}>
-                 {link}
-                </Link>
-              </li>
-            ))}
-          </Flex>
-        <Flex direction={'column'} pl={20} pr={20}>
-            {navLinkGroup2.map((link) => (
-              <li className="nav-link-item">
-                <Link to={"/singlePage"}>
-                 {link}
-                </Link>
-              </li>
-            ))}
-          </Flex>
+      <Menu.Dropdown>
+        <Flex fz={"18px"} direction={"column"} pl={20} pr={20}>
+          {navLinkGroup.map((link) => (
+            <li className="nav-link-item">
+              <Link to={"/singlePage"}>{link}</Link>
+            </li>
+          ))}
+        </Flex>
+        <Flex direction={"column"} pl={20} pr={20}>
+          {navLinkGroup2.map((link) => (
+            <li className="nav-link-item">
+              <Link to={"/singlePage"}>{link}</Link>
+            </li>
+          ))}
+        </Flex>
       </Menu.Dropdown>
     </Menu>
   );
 }
 
-export default Demo
+export default Demo;
